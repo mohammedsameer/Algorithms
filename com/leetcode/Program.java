ffb65e248124be9fc2aa50d19f00c5121eb28af1
msameer <msameer@marinsoftware.com>
2017-10-28 14:20:49 -0700

    Recusrion &  Arrays

--- com/leetcode/Program.java
+++ com/leetcode/Program.java
@@ -1,18 +1,33 @@
 package com.leetcode;
 
+import java.util.*;
 
 /**
  * Algorithm
+ * Sliding window
  */
-class Program {
+public class Program {
 
-     public class Node {
-        int data;
-         Node left;
-         Node right;
-         Node parent;
-         Node(int data) {
-            this.data = data;
+    public static int lengthOfLongestSubstring(String str) {
+        int result = 0;
+        int len = str.length();
+        Set<Character> resultSet = new HashSet<>();
+
+        if(len == 0)
+            return result;
+        int i = 0, j = 0;
+        while(i < len &&  j < len) {
+            if(!resultSet.contains(str.charAt(j))) {
+                resultSet.add(str.charAt(j++));
+                result = Math.max(result, j - i);
+            } else {
+                resultSet.remove(str.charAt(i++));
+            }
         }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("dvdf"));
     }
 }
