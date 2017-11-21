f0ad473bf499d35f78d42f9f16c2ad9cdefec026
msameer <msameer@marinsoftware.com>
2017-11-20 21:19:12 -0800

    Recursive tree traversal

--- com/leetcode/Program.java
+++ com/leetcode/Program.java
@@ -2,9 +2,6 @@ package com.leetcode;
 
 /**
  * Algorithm
- * Recursion
- * 1. root == null (Base condition)
- * 2. root.left == null && root.right == null (Optional base condition)
  */
 public class Program {
 
@@ -14,17 +11,6 @@ public class Program {
       TreeNode right;
       TreeNode(int x) { val = x; }
  }
-
-    public int maxDepth(TreeNode root) {
-        if(root == null)
-            return 0;
-
-        if(root.left == null && root.right == null)
-            return 1;
-
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-    }
-
     public static void main(String[] args) {
        System.out.println();
     }
