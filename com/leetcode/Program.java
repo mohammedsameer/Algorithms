14f9fc106c87b05687153b1ef411ffd3b22a941d
msameer <msameer@marinsoftware.com>
2017-11-21 08:40:38 -0800

    Binary tree traversal and pascal triangle

--- com/leetcode/Program.java
+++ com/leetcode/Program.java
@@ -4,13 +4,7 @@ package com.leetcode;
  * Algorithm
  */
 public class Program {
-
- public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode(int x) { val = x; }
- }
+    
     public static void main(String[] args) {
        System.out.println();
     }
