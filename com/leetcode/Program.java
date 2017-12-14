16ca7793d913af4100e2b79bb663c93b9987d2fd
msameer <msameer@marinsoftware.com>
2017-12-14 08:44:04 -0800

    Linkedlist and Stacks

--- com/leetcode/Program.java
+++ com/leetcode/Program.java
@@ -1,9 +1,28 @@
 package com.leetcode;
 
+
+import java.util.Stack;
+
 /**
- * Algorithm
+ * Algo:
+ * 1. Move elements from pStack to sStack such that they are stored in sorted order in sStack
+ * 2. Exit condition is pStack is empty
  */
 public class Program {
+
+    Stack<Integer> sStack = new Stack<>();
+
+    public void sort(Stack<Integer> pStack) {
+        while (!pStack.isEmpty()) {
+            int value = pStack.pop();
+            while (!sStack.isEmpty() && sStack.peek() < value) {
+                pStack.push(sStack.pop());
+            }
+            sStack.push(value);
+        }
+    }
+
     public static void main(String[] args) {
+
     }
 }
