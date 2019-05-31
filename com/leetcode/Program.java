766b300c48afab98350eb0307e2685222b37ba6d
Mohammed Sameer <mohammed.z.sameer@gmail.com>
2019-05-31 08:13:00 -0700

    More updates

--- com/leetcode/Program.java
+++ com/leetcode/Program.java
@@ -1,46 +1,36 @@
 
 package com.leetcode;
 
+
 import java.util.*;
 
+/**
+ * Algorithm:
+ */
 public class Program {
-    public static String getPermutation(int n, int k) {
-        List<Integer> numbers = new ArrayList<>();
-        int[] factorial = new int[n+1];
-        StringBuilder sb = new StringBuilder();
 
-        // create an array of factorial lookup
-        // factorial[] = {1, 1, 2, 6, 24, ... n!}
-        int sum = 1;
-        factorial[0] = 1;
-        for(int i=1; i<=n; i++){
-            sum *= i;
-            factorial[i] = sum;
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        //Currset helps eliminate duplicates
+        Set<List<Integer>> currSet = new HashSet<>();
+        currSet.add(new ArrayList<>());
+
+        result.addAll(currSet);
+        for(int i=0; i<nums.length; i++) {
+            //Initialize currset from prev result
+            currSet = new HashSet<>(result);
+            for(List<Integer> prev : result) {
+                List<Integer> curr = new ArrayList<>(prev);
+                curr.add(nums[i]);
+                currSet.add(curr);
+            }
+            //We re-initialize and not addall because, add all appends where as re-initializes resets
+            result = new ArrayList<>(currSet);
         }
 
-
-        // create a list of numbers to get indices
-        // numbers = {1, 2, 3, 4}
-        for(int i=1; i<=n; i++){
-            numbers.add(i);
-        }
-
-
-        //k indexed from 0
-        k--;
-
-        for(int i = 1; i <= n; i++){
-            //Divide the permutations into groups of factorial[n-1]
-            int index = k/factorial[n-i];
-            sb.append(String.valueOf(numbers.get(index)));
-            numbers.remove(index);
-            //Eliminate all possible permutations using n-1
-            k=k-index*factorial[n-i];
-        }
-
-        return String.valueOf(sb);
-
+        return result;
     }
+
 
     /**
      * The main method.
@@ -48,6 +38,6 @@ public class Program {
      * @param args the arguments
      */
     public static void main(String[] args) {
-        System.out.println("Permutation Sequence:"+ getPermutation(4,4));
+        subsetsWithDup(new int[]{1,2,2});
     }
 }
