808b63891dabd0f0fc8d63cbd2f5c99fee6490fa
Mohammed Sameer <mohammed.z.sameer@gmail.com>
2019-06-08 12:09:32 -0700

    More updates

--- com/leetcode/Program.java
+++ com/leetcode/Program.java
@@ -2,32 +2,23 @@
 package com.leetcode;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Algorithm:
  */
 public class Program {
 
-    public static int maxProfit(int[] prices) {
-        int firstBuy = Integer.MAX_VALUE;
-        int secondBuy = Integer.MAX_VALUE;
-        int firstSell = 0;
-        int secondSell = 0;
-
-        for(int i=0; i<prices.length; i++) {
-            //First time: You buy and you sell
-            firstBuy = Math.min(firstBuy, prices[i]);
-            firstSell = Math.max(firstSell, prices[i] - firstBuy);
-
-            //Second time: You buy after first sell
-            secondBuy = Math.min(secondBuy, prices[i] - firstSell);
-            secondSell = Math.max(secondSell, prices[i] - secondBuy);
+    public static int singleNumber(int[] nums) {
+        int ones = 0;
+        int twos = 0;
+        //00->01->10
+        //first bit: 0->1->0
+        //sec bit:   0->0->1
+        for(int i=0; i<nums.length; i++) {
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
         }
 
-        return secondSell;
+        return ones;
     }
 
     /**
@@ -36,6 +27,6 @@ public class Program {
      * @param args the arguments
      */
     public static void main(String[] args) {
-        maxProfit(new int[]{3,3,5,0,0,3,1,4});
+        singleNumber(new int[]{1,2,1,1});
     }
 }
