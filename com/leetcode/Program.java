acffc4d3c42ec1291c5b5d71c2c8829924a96b1e
Mohammed Sameer <mohammed.z.sameer@gmail.com>
2019-06-09 18:10:18 -0700

    More updates

--- com/leetcode/Program.java
+++ com/leetcode/Program.java
@@ -1,32 +1,50 @@
 
 package com.leetcode;
 
+import java.util.*;
 
-/**
- * Algorithm:
- */
-public class Program {
 
-    public static int singleNumber(int[] nums) {
-        int ones = 0;
-        int twos = 0;
-        //00->01->10
-        //first bit: 0->1->0
-        //sec bit:   0->0->1
-        for(int i=0; i<nums.length; i++) {
-            ones = (ones ^ nums[i]) & ~twos;
-            twos = (twos ^ nums[i]) & ~ones;
+class Program {
+    // Evaluates the string expr returning the boolean value
+    // using left to right evaluation with no order of operations
+    private boolean evaluate(String expr) {
+        Stack<String> stack = new Stack<>();
+        //Split on space
+        String[] exprArr = expr.split(" ");
+        String leftOperand, rightOperand, op;
+
+        for(String exp : exprArr) {
+            stack.push(exp);
+
+            if (stack.size() > 2) {
+                // pop everything off
+                rightOperand = stack.pop();
+                op = stack.pop();
+                leftOperand = stack.pop();
+
+                // evaluate & push
+                String result;
+                if(op == "|") {
+                    result = String.valueOf(Boolean.valueOf(leftOperand) || Boolean.valueOf(rightOperand));
+                } else {
+                    result = String.valueOf(Boolean.valueOf(leftOperand) && Boolean.valueOf(rightOperand));
+                }
+                stack.push(result);
+            }
         }
-
-        return ones;
+        //Only result on top of stack
+        return Boolean.valueOf(stack.peek());
     }
 
-    /**
-     * The main method.
-     *
-     * @param args the arguments
-     */
+    // A helper for printing out results, do not modify.
+    private void printEvaluate(String expr, boolean expected) {
+        System.out.println("Evaluating " + expr + " -- Actual: " + evaluate(expr) + " Expected: " + expected);
+    }
+
     public static void main(String[] args) {
-        singleNumber(new int[]{1,2,1,1});
+        Program s = new Program();
+        s.printEvaluate("true | false & false", false);
+        s.printEvaluate("true", true);
+        s.printEvaluate("false", false);
     }
 }
