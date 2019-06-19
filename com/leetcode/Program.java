d85fbb4056cf22fc9f0fa6f81973a6f24f3063b7
Mohammed Sameer <mohammed.z.sameer@gmail.com>
2019-06-19 10:39:55 -0700

    More updates

--- com/leetcode/Program.java
+++ com/leetcode/Program.java
@@ -1,12 +1,30 @@
 package com.leetcode;
 
-//Algo:
+import java.util.concurrent.*;
+
 class Program {
 
-//    public static String shortestPalindrome(String s) {
-//        Thread
-//    }
+
+    public static boolean isPowerOfTwo(int n) {
+        if(n==1)
+            return true;
+
+        int[] powers = new int[n/2];
+        powers[0] = 1;
+
+        for(int i=1; i<=n/2; i++) {
+            powers[i] = powers[i-1]*2;
+            if (powers[i] == n)
+                return true;
+            else if (powers[i] > n)
+                return false;
+        }
+        return false;
+    }
 
     public static void main(String[] nums) {
+        isPowerOfTwo(2);
     }
+
 }
+
