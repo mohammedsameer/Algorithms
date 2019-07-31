cdcdec7caa65eaf68087e4f0eb9740c096096622
Mohammed Sameer <mohammed.z.sameer@gmail.com>
2019-07-31 11:41:19 -0700

    More updates

--- com/leetcode/Program.java
+++ com/leetcode/Program.java
@@ -1,21 +1,8 @@
 package com.leetcode;
 
 //Algo:
-//Binary search
 class Program {
-    public int findMinIdx(int[] nums) {
-        int start = 0;
-        int end = nums.length-1;
 
-        while (start<end) {
-            int mid = (start+end)/2;
-            if(nums[mid]>nums[end])
-                start = mid+1;
-            else
-                end = mid;
-        }
-        return nums[start];
-    }
 
     public static void main(String[] args) {
     }
