d920ee6f700cb6b1ea191f05f88290b68c949c42
Mohammed Sameer <mohammed.z.sameer@gmail.com>
2019-08-31 21:33:22 -0700

    Password cracker

--- src/main/java/com/leetcode/Program.java
+++ src/main/java/com/leetcode/Program.java
@@ -1,11 +1,24 @@
 package com.leetcode;
 
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Algo:
  */
 public class Program {
 
+    class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.print("Hello");
     }
 }
